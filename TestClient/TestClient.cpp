4be8a9e51e4c62edb1d4b8d8ce60cf68acb1d537
FanJae <hjjang45@gmail.com>
2026-09-23 18:22:30 +0900

    test: 로그인부터 게임 서버 입장까지 통합 시나리오 확장
    - TestClient가 credential 기반 LoginRequest를 전송하도록 변경
    - 캐릭터 목록 응답 결과를 검증하도록 보완
    - 캐릭터 선택 후 발급된 authKey로 GameServer에 접속
    - EnterGameRequest와 EnterGameResponse 흐름을 통합 테스트
    - 로그인부터 GameSession 인증까지 정상 경로를 하나의 실행으로 검증

--- TestClient/TestClient.cpp
+++ TestClient/TestClient.cpp
@@ -1,4 +1,5 @@
-﻿#include "../LoginServer/LoginPacket.h"
+﻿#include "../Protocol/GamePacket.h"
+#include "../Protocol/LoginPacket.h"
 #include "../ServerCore/Packet.h"
 
 #include <winsock2.h>
@@ -45,8 +46,8 @@ namespace
         return true;
     }
 
-    template<typename T>
-    bool SendPacket(SOCKET socket, LoginPacketOpcode opcode, const T& payload)
+    template<typename TOpcode, typename T>
+    bool SendPacket(SOCKET socket, TOpcode opcode, const T& payload)
     {
         PacketHeader header;
         header.size = sizeof(PacketHeader) + sizeof(T);
@@ -69,8 +70,8 @@ namespace
         return SendAll(socket, reinterpret_cast<const char*>(&header), sizeof(header));
     }
 
-    template<typename T>
-    bool RecvPacket(SOCKET socket, LoginPacketOpcode expectedOpcode, T& payload)
+    template<typename TOpcode, typename T>
+    bool RecvPacket(SOCKET socket, TOpcode expectedOpcode, T& payload)
     {
         PacketHeader header;
 
@@ -94,9 +95,9 @@ int main()
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
         return 1;
 
-    SOCKET socket = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    SOCKET loginSocket = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-    if (socket == INVALID_SOCKET)
+    if (loginSocket == INVALID_SOCKET)
     {
         WSACleanup();
         return 1;
@@ -108,15 +109,15 @@ int main()
 
     if (inet_pton(AF_INET, "127.0.0.1", &address.sin_addr) != 1)
     {
-        closesocket(socket);
+        closesocket(loginSocket);
         WSACleanup();
         return 1;
     }
 
-    if (connect(socket, reinterpret_cast<sockaddr*>(&address), sizeof(address)) == SOCKET_ERROR)
+    if (connect(loginSocket, reinterpret_cast<sockaddr*>(&address), sizeof(address)) == SOCKET_ERROR)
     {
         std::cout << "Connect Failed\n";
-        closesocket(socket);
+        closesocket(loginSocket);
         WSACleanup();
         return 1;
     }
@@ -124,22 +125,23 @@ int main()
     std::cout << "Connected to Login Server\n";
 
     LoginRequest loginRequest;
-    loginRequest.accountId = 1;
+    strcpy_s(loginRequest.loginId, "test");
+    strcpy_s(loginRequest.password, "test1234");
 
-    if (!SendPacket(socket, LoginPacketOpcode::LoginRequest, loginRequest))
+    if (!SendPacket(loginSocket, LoginPacketOpcode::LoginRequest, loginRequest))
     {
         std::cout << "Login Request Failed\n";
-        closesocket(socket);
+        closesocket(loginSocket);
         WSACleanup();
         return 1;
     }
 
     LoginResponse loginResponse;
 
-    if (!RecvPacket(socket, LoginPacketOpcode::LoginResponse, loginResponse))
+    if (!RecvPacket(loginSocket, LoginPacketOpcode::LoginResponse, loginResponse))
     {
         std::cout << "Login Response Failed\n";
-        closesocket(socket);
+        closesocket(loginSocket);
         WSACleanup();
         return 1;
     }
@@ -148,25 +150,34 @@ int main()
 
     if (loginResponse.result != LoginResult::Success)
     {
-        closesocket(socket);
+        closesocket(loginSocket);
         WSACleanup();
         return 1;
     }
 
-    if (!SendEmptyPacket(socket, LoginPacketOpcode::CharacterListRequest))
+    if (!SendEmptyPacket(loginSocket, LoginPacketOpcode::CharacterListRequest))
     {
         std::cout << "Character List Request Failed\n";
-        closesocket(socket);
+        closesocket(loginSocket);
         WSACleanup();
         return 1;
     }
 
     CharacterListResponse characterListResponse;
 
-    if (!RecvPacket(socket, LoginPacketOpcode::CharacterListResponse, characterListResponse))
+    if (!RecvPacket(loginSocket, LoginPacketOpcode::CharacterListResponse, characterListResponse))
     {
         std::cout << "Character List Response Failed\n";
-        closesocket(socket);
+        closesocket(loginSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Character List Result: " << static_cast<int32_t>(characterListResponse.result) << '\n';
+
+    if (characterListResponse.result != CharacterListResult::Success)
+    {
+        closesocket(loginSocket);
         WSACleanup();
         return 1;
     }
@@ -182,33 +193,98 @@ int main()
     CharacterSelectRequest characterSelectRequest;
     characterSelectRequest.characterId = 1001;
 
-    if (!SendPacket(socket, LoginPacketOpcode::CharacterSelectRequest, characterSelectRequest))
+    if (!SendPacket(loginSocket, LoginPacketOpcode::CharacterSelectRequest, characterSelectRequest))
     {
         std::cout << "Character Select Request Failed\n";
-        closesocket(socket);
+        closesocket(loginSocket);
         WSACleanup();
         return 1;
     }
 
     CharacterSelectResponse characterSelectResponse;
 
-    if (!RecvPacket(socket, LoginPacketOpcode::CharacterSelectResponse, characterSelectResponse))
+    if (!RecvPacket(loginSocket, LoginPacketOpcode::CharacterSelectResponse, characterSelectResponse))
     {
         std::cout << "Character Select Response Failed\n";
-        closesocket(socket);
+        closesocket(loginSocket);
         WSACleanup();
         return 1;
     }
 
     std::cout << "Character Select Result: " << static_cast<int32_t>(characterSelectResponse.result) << '\n';
 
-    if (characterSelectResponse.result == CharacterSelectResult::Success)
+    if (characterSelectResponse.result != CharacterSelectResult::Success)
     {
-        std::cout << "AuthKey: " << characterSelectResponse.authKey << '\n';
-        std::cout << "GameServerPort: " << characterSelectResponse.gameServerPort << '\n';
+        closesocket(loginSocket);
+        WSACleanup();
+        return 1;
     }
 
-    closesocket(socket);
+    std::cout << "AuthKey: " << characterSelectResponse.authKey << '\n';
+    std::cout << "GameServerPort: " << characterSelectResponse.gameServerPort << '\n';
+
+    closesocket(loginSocket);
+
+    SOCKET gameSocket = ::socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if (gameSocket == INVALID_SOCKET)
+    {
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in gameAddress{};
+    gameAddress.sin_family = AF_INET;
+    gameAddress.sin_port = htons(characterSelectResponse.gameServerPort);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &gameAddress.sin_addr) != 1)
+    {
+        closesocket(gameSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    if (connect(gameSocket, reinterpret_cast<sockaddr*>(&gameAddress), sizeof(gameAddress)) == SOCKET_ERROR)
+    {
+        std::cout << "Game Server Connect Failed\n";
+        closesocket(gameSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Connected to Game Server\n";
+
+    EnterGameRequest enterGameRequest;
+    enterGameRequest.authKey = characterSelectResponse.authKey;
+
+    if (!SendPacket(gameSocket, GamePacketOpcode::EnterGameRequest, enterGameRequest))
+    {
+        std::cout << "Enter Game Request Failed\n";
+        closesocket(gameSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    EnterGameResponse enterGameResponse;
+
+    if (!RecvPacket(gameSocket, GamePacketOpcode::EnterGameResponse, enterGameResponse))
+    {
+        std::cout << "Enter Game Response Failed\n";
+        closesocket(gameSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Enter Game Result: " << static_cast<int32_t>(enterGameResponse.result) << '\n';
+
+    if (enterGameResponse.result != EnterGameResult::Success)
+    {
+        closesocket(gameSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    closesocket(gameSocket);
     WSACleanup();
     return 0;
 }
